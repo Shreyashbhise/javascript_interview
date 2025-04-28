ae418ddc7eb2bc17d1ae71aadf3c946555bf9497
Shreyash Bhise <108046802+Shreyashbhise@users.noreply.github.com>
2025-04-28 17:43:31 +0530

    Create closure1.js

---
+++ closure1.js
@@ -0,0 +1,27 @@
+for (var i = 0; i < 3; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, 1000);
+}
+🧠 Why this happens?
+var does NOT create a new i for each iteration.
+
+All callbacks share one single i.
+
+At the time callbacks run, i is already 3.
+
+
+
+
+for (let i = 0; i < 3; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, 1000);
+}
+🧠 Why let works?
+let is block-scoped.
+
+Each loop iteration has its own new copy of i.
+
+So each setTimeout remembers the correct value!
+
